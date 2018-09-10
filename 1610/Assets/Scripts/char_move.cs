5509141c39023d7f3defabc8ca82e8337c03ca30
K.P. Sullivan <MacabreSkull@gmail.com>
2018-09-10 17:11:07 -0600

    variables

---
+++ 1610/Assets/Scripts/char_move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class char_move : MonoBehaviour {
+
+public int MoveSpeed;
+public float JumpHeight;
+
+
+	// Use this for initialization
+	void Start () {
+	print("Nope");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
