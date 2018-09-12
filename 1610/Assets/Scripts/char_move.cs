3139e4dc915ee1f849b71e971390991bcc1ab543
K.P. Sullivan <MacabreSkull@gmail.com>
2018-09-12 17:15:17 -0600

    Jumping

--- 1610/Assets/Scripts/char_move.cs
+++ 1610/Assets/Scripts/char_move.cs
@@ -7,14 +7,34 @@ public class char_move : MonoBehaviour {
 public int MoveSpeed;
 public float JumpHeight;
 
-
+//Grounded
+public Transform GroundCheck;
+public float GroundCheckRadius;
+public LayerMask WhatIsGround;
+private bool Grounded;
+//comment
+/*
+1
+2
+3
+ */
 	// Use this for initialization
 	void Start () {
 	print("Nope");
 	}
-	
+	// FixedUpdate is called before Update
+	void FixedUpdate () {
+		Grounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, WhatIsGround);
+	}	
 	// Update is called once per frame
 	void Update () {
-		
+		//Jumping
+		if(Input.GetKeyDown(KeyCode.Space)&& Grounded)
+		{
+			Jump();
+		}
+	}
+			public void Jump(){
+			GetComponent<RigidBody2D>().velocity = new Vector2(GetComponent<RigidBody2D>().velocity.x, JumpHeight);
 	}
 }
